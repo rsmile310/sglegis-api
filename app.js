333ec8701dd2205b2590be46fa508200cea49342
Rsmile310 <rsmile310@gmail.com>
2021-04-24 17:54:33 -0300

    Normalização da api

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
