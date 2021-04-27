dfb30972eda347c0a211011573a05b174f5bc74d
Rsmile310 <rsmile310@gmail.com>
2021-04-27 16:42:54 -0300

    Reorganizando o app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const logger = require('morgan');
-const path = require('path');
-const cors = require('cors');
+const utilitiesConfig = require('./config/utilities');
+const routesConfig = require('./config/routes');
+const databaseConfig = require('./config/database');
+const errorConfig = require('./config/error');
 
+const express = require('express');
 
 const app = express();
+
+databaseConfig(app);
+utilitiesConfig(app);
+routesConfig(app);
+errorConfig(app);
+
+/*app.use('/api/', require('./routes'));
 
 app.use(cors());
 app.use(logger('dev'));
@@ -12,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', require('./routes'));
+*/
 
 module.exports = app;
 
