d54c816f622ec2b57b46669e706cbcebb08770e7
Rsmile310 <rsmile310@gmail.com>
2021-04-27 17:00:28 -0300

    Apontamento para o config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const utilitiesConfig = require('./config/utilities');
 const routesConfig = require('./config/routes');
-const databaseConfig = require('./config/database');
 const errorConfig = require('./config/error');
+//const databaseConfig = require('./config/database');
 
 const express = require('express');
 
@@ -11,16 +11,6 @@ databaseConfig(app);
 utilitiesConfig(app);
 routesConfig(app);
 errorConfig(app);
-
-/*app.use('/api/', require('./routes'));
-
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-*/
 
 module.exports = app;
 
