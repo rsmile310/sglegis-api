7fe0abef8abaebe91df2d4ba60cbe2cbb0b4279b
Rsmile310 <rsmile310@gmail.com>
2021-05-02 18:22:38 -0300

    Migrado documents tables

--- models/documents.js
+++ models/documents.js
@@ -1,13 +1,14 @@
-const { DataTypes } = require('sequelize/types')
 
-module.exports = function (sequelize, Datatypes) {
+module.exports = function (sequelize, DataTypes) {
     return sequelize.define('documents', {
         document_id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
-        document_scope_id: { type: Datatypes.INTEGER, allowNull: false },
-        document_type: { type: Datatypes.TEXT, allowNull: true },
+        document_scope_id: { type: DataTypes.INTEGER, allowNull: false },
+        document_type: { type: DataTypes.TEXT, allowNull: true },
         document_number: { type: DataTypes.TEXT, allowNull: true, defaultValue: 'S/N' },
         document_date: { type: DataTypes.DATE, allowNull: true },
         document_status_id: { type: DataTypes.INTEGER, allowNull: false },
-        document_summary: { type: Datatypes.STRING(2000), allowNull: false },
-    })
-}
+        document_summary: { type: DataTypes.STRING(2000), allowNull: false },
+    }, {
+        tableName: 'documents'
+    });
+};
