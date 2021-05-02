3f45805565004f60ed2ef9a1cebdf614b5606b9b
Rsmile310 <rsmile310@gmail.com>
2021-05-02 18:49:25 -0300

    Rotas para documentos

---
+++ routes/documentScopeRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/documentScopeController');
+
+router.post('/', controller.post);
+router.get('/', controller.getAll);
+router.get('/:id', controller.get);
+router.put('/:id', controller.put);
+router.delete('/:id', controller.delete);
+
+module.exports = router;
