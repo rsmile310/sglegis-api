8651d7e3cbf63911ae2c460c4df8f59ff92c9b94
Rsmile310 <rsmile310@gmail.com>
2021-05-09 16:21:50 -0300

    Remoção do nome fantasia na matriz (customer)

---
+++ migrations/20210509191817_remove-customer_business_name.js
@@ -0,0 +1,71 @@
+const Sequelize = require("sequelize");
+
+/**
+ * Actions summary:
+ *
+ * removeColumn(customer_trade_name) => "customers"
+ *
+ */
+
+const info = {
+  revision: 2,
+  name: "remove-customer_business_name",
+  created: "2021-05-09T19:18:17.113Z",
+  comment: "",
+};
+
+const migrationCommands = (transaction) => [
+  {
+    fn: "removeColumn",
+    params: ["customers", "customer_trade_name", { transaction }],
+  },
+];
+
+const rollbackCommands = (transaction) => [
+  {
+    fn: "addColumn",
+    params: [
+      "customers",
+      "customer_trade_name",
+      {
+        type: Sequelize.STRING(100),
+        field: "customer_trade_name",
+        allowNull: true,
+      },
+      { transaction },
+    ],
+  },
+];
+
+const pos = 0;
+const useTransaction = true;
+
+const execute = (queryInterface, sequelize, _commands) => {
+  let index = pos;
+  const run = (transaction) => {
+    const commands = _commands(transaction);
+    return new Promise((resolve, reject) => {
+      const next = () => {
+        if (index < commands.length) {
+          const command = commands[index];
+          console.log(`[#${index}] execute: ${command.fn}`);
+          index++;
+          queryInterface[command.fn](...command.params).then(next, reject);
+        } else resolve();
+      };
+      next();
+    });
+  };
+  if (useTransaction) return queryInterface.sequelize.transaction(run);
+  return run(null);
+};
+
+module.exports = {
+  pos,
+  useTransaction,
+  up: (queryInterface, sequelize) =>
+    execute(queryInterface, sequelize, migrationCommands),
+  down: (queryInterface, sequelize) =>
+    execute(queryInterface, sequelize, rollbackCommands),
+  info,
+};
