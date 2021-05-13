153bd7a323e428b35d381366883af111dad85bdf
Rsmile310 <rsmile310@gmail.com>
2021-05-13 18:54:19 -0300

    Ajuste de rota para estáticos

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,12 @@
 //const mid = require('../middlewares/AuthHandle');
-
+const path = require('path');
 const routes = require('../routes');
 
 module.exports = (app) => {
+
+    app.use('/', function (req, res) {
+        res.sendFile(path.join(__dirname + '/public/index.html'))
+    });
 
     app.use('/api/', routes);
 };
