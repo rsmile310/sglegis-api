2666af96c285ab07208fc95c96407244366bbd0a
Rsmile310 <rsmile310@gmail.com>
2021-05-15 15:59:31 -0300

    Listas de ordem pratica e conformidade

---
+++ src/enuns/conformity.js
@@ -0,0 +1,9 @@
+const conformity = {
+    A_VERIFICAR: "A VERIFICAR",
+    SIM: "SIM",
+    NAO: "NÃO",
+    NAO_APLICA: "NÃO SE APLICA",
+    FUTURO: "FUTURO",
+    PARCIAL: "PARCIAL",
+}
+module.exports = { conformity };
