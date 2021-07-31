6ed8dc73089720f84fde9721ac328c248087bebf
Rsmile310 <rsmile310@gmail.com>
2021-07-31 16:19:08 -0300

    Bug fixed: not showing aspects when add new item

--- src/controllers/documentItemController.js
+++ src/controllers/documentItemController.js
@@ -11,8 +11,7 @@ exports.getAreasAspects = (req, res, next) => {
     let sql = `select ap.*, ar.area_name, iaa.item_area_aspect_id
                 from areas_aspects ap
                 join areas ar on (ap.area_id = ar.area_id)
-                left join items_areas_aspects iaa on (iaa.area_id = ar.area_id and iaa.area_aspect_id = ap.area_aspect_id)
-                where (iaa.document_item_id = ${req.params.id} or iaa.document_item_id is null)
+                left join items_areas_aspects iaa on (iaa.area_id = ar.area_id and iaa.area_aspect_id = ap.area_aspect_id) and (iaa.document_item_id = ${req.params.id} or iaa.document_item_id is null)
                order by ar.area_id, ap.area_aspect_name`;
 
     db.sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(values => {
