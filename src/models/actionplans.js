0eeb43f60c3f921b6197c85dbc1f1a6246410ee1
Rsmile310 <rsmile310@gmail.com>
2021-08-07 15:14:05 -0300

    Added created and updated properties for model for action plan feature

--- src/models/actionplans.js
+++ src/models/actionplans.js
@@ -5,7 +5,15 @@ module.exports = function (sequelize, DataTypes) {
         activity: { type: DataTypes.TEXT, allowNull: false, comment: 'The activity description' },
         responsible: { type: DataTypes.STRING(50), allowNull: false, comment: 'The name of responsible`s action plan' },
         email: { type: DataTypes.STRING(50), allowNull: true, comment: 'Email`s resposible' },
-        deadline: { type: DataTypes.DATE, allowNull: true, comment: 'The date responsible must finish the task' }
+        deadline: { type: DataTypes.DATE, allowNull: true, comment: 'The date responsible must finish the task' },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
     }, {
         tableName: 'actionplans'
     });
