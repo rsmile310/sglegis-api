715da4ed3e6db6d330d6f0112b0b4d3defb5d960
Rsmile310 <rsmile310@gmail.com>
2021-08-07 15:16:28 -0300

    Added status to the action plan

--- src/models/actionplans.js
+++ src/models/actionplans.js
@@ -5,6 +5,7 @@ module.exports = function (sequelize, DataTypes) {
         activity: { type: DataTypes.TEXT, allowNull: false, comment: 'The activity description' },
         responsible: { type: DataTypes.STRING(50), allowNull: false, comment: 'The name of responsible`s action plan' },
         email: { type: DataTypes.STRING(50), allowNull: true, comment: 'Email`s resposible' },
+        status: { type: DataTypes.INTEGER, default: 0, allowNull: false, comment: '0:new/open; 2:closed' },
         deadline: { type: DataTypes.DATE, allowNull: true, comment: 'The date responsible must finish the task' },
         createdAt: {
             type: DataTypes.DATE,
