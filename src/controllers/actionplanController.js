d919e44b52df0c5e0254ae78033bedb4b3d06313
Rsmile310 <rsmile310@gmail.com>
2021-08-07 21:29:45 -0300

    Added actionplan models

--- src/controllers/actionplanController.js
+++ src/controllers/actionplanController.js
@@ -1,27 +1,75 @@
 const { actionplans } = require('../models');
 const base = require('./baseController');
+const db = require('../models/index');
+const sequelize = require('sequelize');
 
 exports.getAll = (req, res, next) => {
-    base.getAll(actionplans, req, res, next);    
+    //base.getAll(actionplans, req, res, next);    
+    let ret = [];
+    let sql = `select * 
+                from actionplans a
+                inner join actionplan_items ai on a.actionplan_id = ai.actionplan_id
+                where a.unit_id = ${req.params.unit_id} 
+                and a.item_area_aspect_id = ${req.params.item_area_aspect_id} 
+                and a.actionplan_id = ${req.params.actionplan_id} 
+                and status != 3;`; //all actionplan but not deleted
+
+    db.sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(values => {
+        res.send(values);
+    });
 }
 
 exports.getQuery = (req, res, next)=>{
-    base.query(actionplans, req, res, next);
+    //base.query(actionplans, req, res, next);
+
+    let ret = [];
+    let sql = `select * 
+                from actionplans a
+                inner join actionplan_items ai on a.actionplan_id = ai.actionplan_id
+                where a.unit_id = ${req.query.unit_id} 
+                and a.item_area_aspect_id = ${req.query.item_area_aspect_id} 
+                and a.actionplan_id = ${req.query.actionplan_id} 
+                and status != 3;`; //all actionplan but not deleted
+
+    db.sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(values => {
+        res.send(values);
+    });
 }
 
-exports.get = (req, res, next) => {
-    base.get(actionplans, req, res, next, 'actionplan_id');
-};
 
 exports.post = (req, res, next) => {
-    base.insert(actionplans, req, res, next);
+    //base.insert(actionplans, req, res, next);
+
+    let actionplans = req.body;
+
+    //if there is no id, so (insert)
+    if (actionplans.actionplan_id <= 0)
+        insert(actionplans, res);
+    else
+        update(actionplans, res);
+};
+
+function insert(actionplans, res) {
+    db.sequelize.transaction(function (t) {
+        return db.actionplans.create(actionplans, { transaction: t }).then(function (new_action_plan) {
+            var items = actionplans.actionplan_items;
+            items.forEach(i => {
+                i.actionplan_id = new_action_plan.actionplan_id;
+            });
+            return db.actionplan_items.bulkCreate(items, { transaction: t }).then(function (new_item) {
+                res.send(new_action_plan);
+            });
+        });
+    });
 }
 
-exports.put = (req, res, next) => {
-    base.update(actionplans, req, res, next, 'actionplan_id');
-}
-
-exports.delete = (req, res, next) => {
-    base.delete(actionplans, req, res, next, 'actionplan_id');
-}
-
+function update (actionplans, res) {
+    db.sequelize.transaction(function (t) {
+        return db.actionplans.update(actionplans, { transaction: t, where:{actionplan_id: actionplans.actionplan_id} }).then(function (new_action_plan) {
+            var items = actionplans.actionplan_items;
+            return db.actionplan_items.bulkCreate(items, { transaction: t, updateOnDuplicate: ["updatedAt", "status"] }).then(function (new_item) {
+                res.send(new_action_plan);
+            });
+        });
+    });
+};
--- src/models/actionplans.js
+++ src/models/actionplans.js
@@ -1,12 +1,10 @@
 
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('actionplans', {
-        actionplan_id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
-        activity: { type: DataTypes.TEXT, allowNull: false, comment: 'The activity description' },
-        responsible: { type: DataTypes.STRING(50), allowNull: false, comment: 'The name of responsible`s action plan' },
-        email: { type: DataTypes.STRING(50), allowNull: true, comment: 'Email`s resposible' },
-        status: { type: DataTypes.INTEGER, default: 0, allowNull: false, comment: '0:new/open; 2:closed' },
-        deadline: { type: DataTypes.DATE, allowNull: true, comment: 'The date responsible must finish the task' },
+        actionplan_id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement:true },
+        unit_id: {type:DataTypes.INTEGER, allowNull: false, comment: 'The unit'},
+        item_area_aspect_id: {type:DataTypes.INTEGER, allowNull: false, comment: 'The item and aspect matched on unit'},
+        user_id:{type:DataTypes.INTEGER, allowNull: false, comment: 'The user logged who inserted the action plan'},
         createdAt: {
             type: DataTypes.DATE,
             allowNull: true
